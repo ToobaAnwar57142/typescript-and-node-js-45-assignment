ee0745b08d0cf2d9ee8a8b0b6dfb2959f33ab3e6
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 03:52:09 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
-var magician = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
+//Unchanged Magicians: Start with your work from Exercise 40. Call let magician : string[] = ['Harry Potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var magician = ['Harry Potter', 'Herminoe Granger', 'Ron Weasley', 'Albus Dumbledore'];
+function copyArray(arr) {
+    return __spreadArray([], arr, true);
+}
 function make_great(magicanArry) {
     for (var i = 0; i < magicanArry.length; i++) {
         magician[i] = 'the Great' + magicanArry[i];
@@ -10,4 +22,9 @@ function show_magicians(magicans) {
         console.log(element);
     });
 }
+var copyMagicianArray = copyArray(magician);
+make_great(copyMagicianArray);
+console.log('\n\nthis is my orignal array:');
 show_magicians(magician);
+console.log('\n\nThis is my modfied copy of the array:');
+show_magicians(copyMagicianArray);
--- main.ts
+++ main.ts
@@ -1,25 +1,38 @@
-//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+//Unchanged Magicians: Start with your work from Exercise 40. Call let magician : string[] = ['Harry Potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
 
 
 
-let magician : string[] = ['Harry Potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
+let magician : string[] =['Harry Potter','Herminoe Granger','Ron Weasley','Albus Dumbledore'];
 
-
-function make_great (magicanArry: string[]){
-
-     for(let i=0; i<magicanArry.length; i++){
-
-        magician[i] = 'the Great' + magicanArry[i]
-     }
-     
+function copyArray(arr:string[]){
+    return[...arr]
 }
+
+
+function make_great (magicanArry:string[]){
+
+       for(let i=0; i<magicanArry.length; i++){
+
+              magician[i] = 'the Great' + magicanArry[i]
+       }
+
+}     
 
 function show_magicians(magicans: string[]){
 
-magician.forEach(element => {
+    magician.forEach(element => {
         console.log(element);
-    
     });
 }
 
+
+const copyMagicianArray = copyArray (magician);
+
+make_great(copyMagicianArray);
+
+console.log('\n\nthis is my orignal array:');
 show_magicians(magician);
+
+console.log ('\n\nThis is my modfied copy of the array:');
+show_magicians(copyMagicianArray);
+
