971813ea1b032bd44b86dc922aaca36d574ba070
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:35:10 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.*/
-var array = ['Anwar', 'Muhammad', 'Sakina', 5, 7, 14, 2];
-console.log(array[5]);
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
-console.log(array[4]);
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:*/
+/*let car = 'subaru';*/
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+/*• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+//Test No :01
+console.log("is car =='subaru? I predict True.");
+console.log(car === 'subaru');
+//Task No :02
+console.log("Is car =='subaru'? I predict True.");
+console.log(car == 'subaru');
+//Task No :03
+console.log("Is car I='Honda'? I predict True'");
+console.log(car != 'Honda');
+//Task No :04
+console.log("Is car !== 'found? I predict True.");
+console.log(car !== 'ford');
+//Task No :05
+console.log("Is car.Upper case == 'SUBARU'? I predict True.");
+console.log(car == 'SUBARU');
+//Task No:06
+console.log("Is car == 'SUBARU'? I predict False.");
+console.log(car == 'SUBARU');
+//Task No:07
+console.log("Is car === 'SUBARU'? I predict False.");
+console.log(car === 'SUBARU');
+//Task No:08
+console.log("Is car === 'SUBARU'? I predict False.");
+console.log(car === 'Tooba');
+//Task No:09
+console.log("Is car=== 'Bus'? I predict False.");
+console.log(car === 'Bus');
+//Task No:10
+console.log("Is car === 'Cycle'? I predoct False.");
+console.log(car === 'Cycle');
