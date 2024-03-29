7312217f0ee8e5c961914c190a9fe74c79c5f704
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:11:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-//Excerise 09:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.//
-//Excerise 10(A) Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.//
-//Tooba Anwar
-//wednesday,feb-4-2025
-//store my favorite number in a variable
-var Favorite_Number = 57142;
-var message = ("My favorite number is,".concat(Favorite_Number));
-console.log(message);
+// Excerise 11: Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.//
+//Excerise 10(B) : Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.//
+//TOOBA ANWAR
+//Thursday 2-16-2024
+//Storing friends names in an Array
+var names = ["Sakina", "Muhammad", "Zain", "Fareeha"];
+//prinitg names one by one
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
