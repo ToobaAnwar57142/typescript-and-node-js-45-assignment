2e201b847537582cf3d6ac9e7940a92acabde66a
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:07:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//Excerise 06: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces./
-var Namewithwhitespace = "\t\n    Tooba Anwar     \t\n";
-console.log(Namewithwhitespace);
-var Namewithoutwithspace = Namewithwhitespace.trim();
-console.log(Namewithoutwithspace);
+//Excerise 07: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
