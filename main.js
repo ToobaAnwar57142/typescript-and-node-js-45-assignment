0d780d7ba48f2205053082d7d7f2493036865944
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:17:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-"use strict";
-//Excerise 12: Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.//
-Object.defineProperty(exports, "__esModule", { value: true });
-var names = ["Sakina", "Muhammad", "Zain", "Fareeha"];
-console.log("Make Sure To Subscribe To Our Channel, ".concat(names[0]));
-console.log("Make Sure To Subscribe To Our Channel, ".concat(names[1]));
-console.log("Make Sure To Subscribe To Our Channel, ".concat(names[2]));
-console.log("Make Sure To Subcribe  To Our Channel, ".concat(names[3]));
+//Excerise 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”//
+var transportation = ["Heavybike", "Car", "Bicycle", "Aeroplane"];
+console.log("\u201CI would like to own a \u201D : ".concat(transportation[0]));
+console.log("\u201CI would like to own a \u201D : ".concat(transportation[1]));
+console.log("\u201CI would like to own a \u201D : ".concat(transportation[2]));
+console.log("\u201CI would like to own a \u201D : ".concat(transportation[3]));
--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-//Excerise 12: Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.//
-
-import { subscribe } from "diagnostics_channel";
-
-let names: string [] = ["Sakina","Muhammad", "Zain", "Fareeha"];
+//Excerise 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”//
 
 
-console.log(`Make Sure To Subscribe To Our Channel, ${names[0]}`);
-console.log(`Make Sure To Subscribe To Our Channel, ${names[1]}`);
-console.log(`Make Sure To Subscribe To Our Channel, ${names[2]}`);
-console.log(`Make Sure To Subcribe  To Our Channel, ${names[3]}`);
+let transportation: string[] = ["Heavybike" , "Car" , "Bicycle" ,"Aeroplane"];
+
+console.log(`“I would like to own a ” : ${transportation[0]}`);
+console.log(`“I would like to own a ” : ${transportation[1]}`);
+console.log(`“I would like to own a ” : ${transportation[2]}`);
+console.log(`“I would like to own a ” : ${transportation[3]}`);
