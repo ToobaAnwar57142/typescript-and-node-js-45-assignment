98b69bb258d2706532c7e0a538f25ebbae98b636
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 03:00:53 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default:country
-function describe_city(city, country) {
-    if (country === void 0) { country = 'Default_Country.'; }
-    console.log(" ".concat(city, " is in ").concat(country));
+//City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+//"Lahore, Pakistan"
+//Call your function with at least three city-country pairs, and print the value that’s returned.
+function city_country(city, country) {
+    return "".concat(city, ",").concat(country);
 }
-describe_city('Karachi', 'Pakistan');
-describe_city('Tokyo', 'Japan');
-describe_city('Paris');
+console.log(city_country('Karachi', 'Pakistan'));
+console.log(city_country('Tokyo', 'Japan'));
+console.log(city_country('Paris', 'France'));
