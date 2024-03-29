2a9eaa1be1863aa1da29215f49bf1be5d49f4387
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 01:59:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var personName = "Tooba Anwar";
+var message = "Hello ".concat(personName, ",Would you like to learn TypeScript today?");
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Excerise 2: Personal Message: Store a person's name in a variable,and print a message to that person.Your message should be simple, such as, "Hellow Eric,would you like to learn some Phython today?"
+
+
+let personName: string = "Tooba Anwar"
+let message: string =`Hello ${personName},Would you like to learn TypeScript today?`
+console.log(message)
