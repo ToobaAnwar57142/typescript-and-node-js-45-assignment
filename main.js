32c75e827de9b592a6e24f1e7925f64301079265
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:10:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-var favoriteNumber = 57142;
-console.log("My favorite number is ".concat(favoriteNumber));
+//Excerise 09:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.//
+//Excerise 10(A) Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.//
+//Tooba Anwar
+//wednesday,feb-4-2025
+//store my favorite number in a variable
+var Favorite_Number = 57142;
+var message = ("My favorite number is,".concat(Favorite_Number));
+console.log(message);
--- main.ts
+++ main.ts
@@ -1,2 +1,11 @@
-let favoriteNumber: number= 57142;
-console.log(`My favorite number is ${favoriteNumber}`);
+//Excerise 09:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.//
+
+//Excerise 10(A) Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.//
+
+//Tooba Anwar
+//wednesday,feb-4-2025
+
+//store my favorite number in a variable
+const Favorite_Number: number = 57142;
+const message: string = (`My favorite number is,${Favorite_Number}`);
+console.log(message);
