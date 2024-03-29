4c930bc3168922b8acc1a0228614761b168d6572
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:30:10 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
-//Excerise 20: Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+//They think of something you could store in a TypeScript Object. 
+//Write a program that creates Objects containing these items.
 
-let languages : string [] = ['english','urdu','french','arabic','Hindhi']
-console.log('list of languages:')
-for(let top of languages){
-    console.log(top)
-
+interface person{
+    age: number,
+    name: string,
+    nationality: string,
+    student : Boolean
 }
+
+let person :person = {
+
+    age : 14,
+    name :'Tooba',
+    nationality : 'Pakistan',
+    student : true,
+}
+//print person
+console.log(person);
+
+
+//Datatype of car object
+interface car{
+    maker : string,
+    color : string,
+    automatic : boolean,
+}
+//car object
+let car = {
+    maker : 'toyota',
+    color : 'Black',
+    automatic : true
+}
+
+//print car
+console.log(car);
