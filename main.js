87f254f62fefe33706419ed38d3f20366450dfb1
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:01:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-var personName = "Tooba Anwar";
-var message = "Hello ".concat(personName, ",Would you like to learn TypeScript today?");
-console.log(message);
+//Excerise 3:Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var PersonName = "Tooba Anwar";
+//step 1 show in lower case:
+var lowercaseName = PersonName.toLowerCase();
+console.log(lowercaseName);
+//step 2 show name in uppercase
+var uppercaseName = PersonName.toUpperCase();
+console.log(uppercaseName);
+//step 3 show namein titlecase
+var words = PersonName.split(" ");
+var titlecaseName = "";
+for (var i = 0; i < words.length; i++) {
+    titlecaseName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLocaleLowerCase() + " ";
+}
+;
+console.log(titlecaseName);
--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
-//Excerise 2: Personal Message: Store a person's name in a variable,and print a message to that person.Your message should be simple, such as, "Hellow Eric,would you like to learn some Phython today?"
+//Excerise 3:Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 
+let PersonName: string = "Tooba Anwar";
 
-let personName: string = "Tooba Anwar"
-let message: string =`Hello ${personName},Would you like to learn TypeScript today?`
-console.log(message)
+//step 1 show in lower case:
+let lowercaseName: string = PersonName.toLowerCase();
+console.log(lowercaseName);
+
+//step 2 show name in uppercase
+let uppercaseName: string = PersonName.toUpperCase();
+console.log(uppercaseName);
+
+//step 3 show namein titlecase
+let words: string[] =PersonName.split(" ");
+let titlecaseName: string = ""
+for (let i = 0 ; i <words.length; i++) {
+titlecaseName +=words[i].charAt(0).toUpperCase() +words[i].slice(1).toLocaleLowerCase()+" "
+};
+
+console.log(titlecaseName)
