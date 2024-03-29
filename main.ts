3f8392fbae045f5aa5bd716f7f1fdc1f53b89f42
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 02:40:55 -0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,57 +1,40 @@
-//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain
- //• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+//Stages of Life: Write an if-else chain that determines a person’s stage of life. 
+//Set a value for the variable age, and then:
+let age: number = 25;
 
-let alienColors: string = "green";
-
-//• If the alien is green, print a message that the player earned 10 points.
-//• If the alien is yellow, print a message that the player earned 10 points.
-//• If the alien is red, print a message that the player earned 15 points.
-
-//•Version 1 of the program 
-if (alienColors === "green"){
-     console.log("player earned 5 points");
+//• If the person is less than 2 years old, print a message that the person is a baby.
+//• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+if (age < 2){
+    console.log("person is a baby");
 }
-else if(alienColors === "yellow"){
-    console.log("player earned 10 points");
+else if(age >= 2 && age < 4){
+    console.log("person is a toodler");
 }
-else if(alienColors === "red"){
-    console.log("player earned 15 points");
+else if(age >4 && age <13){
+    console.log("person is a kid");
 }
-else{
-    console.log("Please select right color");
+else if(age >13 && age <20){
+    console.log("person is a teenager");
 }
-
-//•Version 2 of the program 
-alienColors = "yellow";
-
-if (alienColors === "green"){
-    console.log("player earned 5 points");
+else if(age >13 && age <20){
+    console.log("person is an adult");
 }
-else if(alienColors === "yellow"){
-   console.log("player earned 10 points");
-}
-else if(alienColors === "red"){
-   console.log("player earned 15 points");
-}
-else{
-   console.log("Please select right color");
-}
-
-//•Version 2 of the program 
-alienColors = "red";
-
-if (alienColors === "green"){
-    console.log("player earned 5 points");
-}
-else if(alienColors === "yellow"){
-   console.log("player earned 10 points");
-}
-else if(alienColors === "red"){
-   console.log("player earned 15 points");
-}
-else{
-   console.log("Please select right color");
+else if(age >13 && age <20){
+    console.log("person is a teenager")
 }
 
 
 
+
+
+
+
+
+
+//• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+
+//• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+
+//• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+
+//• If the person is age 65 or older, print a message that the person is an elder.
