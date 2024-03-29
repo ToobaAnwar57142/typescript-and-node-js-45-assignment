51466dec686022cd6be399379fe64bbb6899a1b6
ToobaAnwar57142 <160787120+ToobaAnwar57142@users.noreply.github.com>
2024-03-29 03:51:25 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
 var magician = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Albus Dumbledore'];
-function show_magicians(magican) {
+function make_great(magicanArry) {
+    for (var i = 0; i < magicanArry.length; i++) {
+        magician[i] = 'the Great' + magicanArry[i];
+    }
+}
+function show_magicians(magicans) {
     magician.forEach(function (element) {
         console.log(element);
     });
--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
-//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+//Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+
 
 
 let magician : string[] = ['Harry Potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
 
-function show_magicians(magican: string[]){
 
-    magician.forEach(element => {
+function make_great (magicanArry: string[]){
+
+     for(let i=0; i<magicanArry.length; i++){
+
+        magician[i] = 'the Great' + magicanArry[i]
+     }
+     
+}
+
+function show_magicians(magicans: string[]){
+
+magician.forEach(element => {
         console.log(element);
     
     });
 }
 
 show_magicians(magician);
-
-
-
